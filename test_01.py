82c689296bb4f9aa9a104f39c34d2657b91aa478
NayanPython <choudharyji527@gmail.com>
2026-06-04 14:23:24 +0530

    initalised cv2 , changed image properties.

--- test_01.py
+++ test_01.py
@@ -18,3 +18,14 @@ def process_medicine_images(image_paths):
         if not os.path.exists(path):
             return f"ERROR: File missing -> {path}"
 
+        # OpenCV Preprocessing
+        img = cv2.imread(path)
+        height, width = img.shape[:2]
+        max_dim = 1024
+        if width > max_dim or height > max_dim:
+            scaling_factor = max_dim / float(max(width, height))
+            img = cv2.resize(img, None, fx=scaling_factor, fy=scaling_factor, interpolation=cv2.INTER_AREA)
+
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        cleaned_images.append(Image.fromarray(img_rgb))
+
