16599e3086c1ca5251c78e785a1141db9f42c0d6
NayanPython <choudharyji527@gmail.com>
2026-06-04 14:28:44 +0530

    connected db and created columns

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 
+import sqlite3
+
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from google import genai
@@ -10,4 +12,30 @@ load_dotenv()
 API_KEY = os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key=API_KEY)
 app = FastAPI()
+
+# --- DATABASE ARCHITECTURE ---
+DB_FILE = "medicines.db"
+
+
+def init_db():
+    print("Initializing SQLite Database...")
+    conn = sqlite3.connect(DB_FILE)
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS scanned_medicines (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            medicine_name TEXT,
+            expiry_date TEXT,
+            manufacture_date TEXT,
+            company TEXT,
+            scan_timestamp TEXT
+        )
+    ''')
+    conn.commit()
+    conn.close()
+
+
+# Run the DB init on startup
+init_db()
+
 
