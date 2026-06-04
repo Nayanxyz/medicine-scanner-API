bb2d28f4e06c88e69361523eeaa3e5adea94dd13
NayanPython <choudharyji527@gmail.com>
2026-06-04 14:30:16 +0530

    reshaped image properties

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
-
+import cv2
 import sqlite3
-
+import numpy as np
+from PIL import Image
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from google import genai
+
 
 
 # --- INITIALIZATION ---
@@ -37,5 +39,19 @@ def init_db():
 
 # Run the DB init on startup
 init_db()
+
+
+def clean_and_prepare_image(file_bytes):
+    nparr = np.frombuffer(file_bytes, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+    height, width = img.shape[:2]
+    max_dim = 1024
+    if width > max_dim or height > max_dim:
+        scaling_factor = max_dim / float(max(width, height))
+        img = cv2.resize(img, None, fx=scaling_factor, fy=scaling_factor, interpolation=cv2.INTER_AREA)
+
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return Image.fromarray(img_rgb)
 
 
