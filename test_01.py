f4d802df2f97733314880afa943c9a488232cb19
NayanPython <choudharyji527@gmail.com>
2026-06-04 14:22:37 +0530

    created function

--- test_01.py
+++ test_01.py
@@ -9,3 +9,12 @@ load_dotenv()
 API_KEY = os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key=API_KEY)
 
+
+def process_medicine_images(image_paths):
+    cleaned_images = []
+
+    for path in image_paths:
+        print(f"Processing: {os.path.abspath(path)}")
+        if not os.path.exists(path):
+            return f"ERROR: File missing -> {path}"
+
